ece4b0a8e76da3783a018aedc96ab245febcfc11
Chasmical <kaz@chsm.dev>
2025-03-13 18:42:23 +0700

    🚚 Rename and move DeclBuffer to InflectionBuffer

--- Chasm.Grammar.Russian/Nouns/RussianNoun.Declension.cs
+++ Chasm.Grammar.Russian/Nouns/RussianNoun.Declension.cs
@@ -33,7 +33,7 @@ namespace Chasm.Grammar.Russian
             // Allocate some memory for string manipulations
             const int extraCharCount = 8;
             Span<char> buffer = stackalloc char[stem.Length + extraCharCount];
-            DeclBuffer results = new(buffer);
+            InflectionBuffer results = new(buffer);
 
             // Find the appropriate noun ending
             ReadOnlySpan<char> ending = DetermineNounEnding(declension, props);
@@ -58,7 +58,7 @@ namespace Chasm.Grammar.Russian
             return results.Result.ToString();
         }
 
-        private static void ProcessUniqueAlternation(ref DeclBuffer buffer, NounDecl declension, NounProps props)
+        private static void ProcessUniqueAlternation(ref InflectionBuffer buffer, NounDecl declension, NounProps props)
         {
             Span<char> stem = buffer.Stem;
 
@@ -177,7 +177,7 @@ namespace Chasm.Grammar.Russian
             }
         }
 
-        private static void ProcessVowelAlternation(ref DeclBuffer buffer, NounDecl declension, NounProps props)
+        private static void ProcessVowelAlternation(ref InflectionBuffer buffer, NounDecl declension, NounProps props)
         {
             if (props.Gender == RussianGender.Masculine || props.Gender == RussianGender.Feminine && declension.StemType == 8)
             {
@@ -296,7 +296,7 @@ namespace Chasm.Grammar.Russian
             }
         }
 
-        private static void ProcessYoAlternation(ref DeclBuffer buffer, NounDecl declension, NounProps props)
+        private static void ProcessYoAlternation(ref InflectionBuffer buffer, NounDecl declension, NounProps props)
         {
             // Alternating ё is handled in the ° method
             if ((declension.Flags & RussianDeclensionFlags.Circle) != 0) return;
@@ -328,78 +328,6 @@ namespace Chasm.Grammar.Russian
                 {
                     buffer.Buffer[letterIndex] = 'ё';
                 }
-            }
-        }
-
-        private ref struct DeclBuffer
-        {
-            public readonly Span<char> Buffer;
-            public int StemLength;
-            private int ResultLength;
-
-            public DeclBuffer(Span<char> buffer)
-                => Buffer = buffer;
-
-            public readonly Span<char> Stem => Buffer[..StemLength];
-            public readonly Span<char> Ending => Buffer[StemLength..ResultLength];
-            public readonly Span<char> Result => Buffer[..ResultLength];
-
-            public void WriteInitialParts(ReadOnlySpan<char> stem, ReadOnlySpan<char> ending)
-            {
-                StemLength = stem.Length;
-                ResultLength = StemLength + ending.Length;
-
-                stem.CopyTo(Buffer);
-                ending.CopyTo(Buffer[stem.Length..]);
-            }
-
-            public void RemoveStemCharAt(int index)
-            {
-                for (int i = index + 1; i < ResultLength; i++)
-                    Buffer[i - 1] = Buffer[i];
-                StemLength--;
-                ResultLength--;
-            }
-            public void ShrinkStemBy(int offset)
-            {
-                StemLength -= offset;
-                ResultLength -= offset;
-                for (int i = StemLength; i < ResultLength; i++)
-                    Buffer[i] = Buffer[i + offset];
-            }
-            public void AppendToStem(char a, char b)
-            {
-                for (int i = ResultLength - 1; i >= StemLength; i--)
-                    Buffer[i + 2] = Buffer[i];
-                Buffer[StemLength] = a;
-                Buffer[StemLength + 1] = b;
-                StemLength += 2;
-                ResultLength += 2;
-            }
-
-            public void RemoveEnding()
-                => ResultLength = StemLength;
-            public void ReplaceEnding(char a)
-            {
-                Buffer[StemLength] = a;
-                ResultLength = StemLength + 1;
-            }
-            public void ReplaceEnding(char a, char b)
-            {
-                Buffer[StemLength] = a;
-                Buffer[StemLength + 1] = b;
-                ResultLength = StemLength + 2;
-            }
-
-            public void InsertBetweenTwoLastStemChars(char ch)
-            {
-                for (int i = ResultLength; i >= StemLength; i--)
-                    Buffer[i] = Buffer[i - 1];
-
-                Buffer[StemLength - 1] = ch;
-
-                StemLength++;
-                ResultLength++;
             }
         }
 
---
+++ Chasm.Grammar.Russian/Shared/InflectionBuffer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Chasm.Grammar.Russian
+{
+    internal ref struct InflectionBuffer
+    {
+        public readonly Span<char> Buffer;
+        public int StemLength;
+        private int ResultLength;
+
+        public InflectionBuffer(Span<char> buffer)
+            => Buffer = buffer;
+
+        public readonly Span<char> Stem => Buffer[..StemLength];
+        public readonly Span<char> Ending => Buffer[StemLength..ResultLength];
+        public readonly Span<char> Result => Buffer[..ResultLength];
+
+        public void WriteInitialParts(ReadOnlySpan<char> stem, ReadOnlySpan<char> ending)
+        {
+            StemLength = stem.Length;
+            ResultLength = StemLength + ending.Length;
+
+            stem.CopyTo(Buffer);
+            ending.CopyTo(Buffer[stem.Length..]);
+        }
+
+        public void RemoveStemCharAt(int index)
+        {
+            for (int i = index + 1; i < ResultLength; i++)
+                Buffer[i - 1] = Buffer[i];
+            StemLength--;
+            ResultLength--;
+        }
+        public void ShrinkStemBy(int offset)
+        {
+            StemLength -= offset;
+            ResultLength -= offset;
+            for (int i = StemLength; i < ResultLength; i++)
+                Buffer[i] = Buffer[i + offset];
+        }
+        public void AppendToStem(char a, char b)
+        {
+            for (int i = ResultLength - 1; i >= StemLength; i--)
+                Buffer[i + 2] = Buffer[i];
+            Buffer[StemLength] = a;
+            Buffer[StemLength + 1] = b;
+            StemLength += 2;
+            ResultLength += 2;
+        }
+
+        public void RemoveEnding()
+            => ResultLength = StemLength;
+        public void ReplaceEnding(char a)
+        {
+            Buffer[StemLength] = a;
+            ResultLength = StemLength + 1;
+        }
+        public void ReplaceEnding(char a, char b)
+        {
+            Buffer[StemLength] = a;
+            Buffer[StemLength + 1] = b;
+            ResultLength = StemLength + 2;
+        }
+
+        public void InsertBetweenTwoLastStemChars(char ch)
+        {
+            for (int i = ResultLength; i >= StemLength; i--)
+                Buffer[i] = Buffer[i - 1];
+
+            Buffer[StemLength - 1] = ch;
+
+            StemLength++;
+            ResultLength++;
+        }
+    }
+}
