c2c78134d1f6f9da7592a616de6472c51206c107
Chasmical <kaz@chsm.dev>
2025-03-15 04:27:17 +0700

    ✅ Start adding RussianAdjective declension tests

---
+++ Chasm.Grammar.Tests/Russian/Adjective.Declension.Fixtures.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using Chasm.Grammar.Russian;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Chasm.Grammar.Tests
+{
+    public partial class RussianAdjectiveTests
+    {
+        [Pure] public static FixtureAdapter<DeclensionFixture> CreateDeclensionFixtures()
+        {
+            FixtureAdapter<DeclensionFixture> adapter = [];
+
+            DeclensionFixture New(string stem, string info)
+                => adapter.Add(new DeclensionFixture(stem, info));
+
+            #region Simple stem types 0 through 7
+
+            New("хаки", "п 0")
+                .Returns("хаки, хаки, хаки, хаки, хаки, хаки, хаки")
+                .Returns("хаки, хаки, хаки, хаки, хаки, хаки, хаки")
+                .Returns("хаки, хаки, хаки, хаки, хаки, хаки, хаки")
+                .Returns("хаки, хаки, хаки, хаки, хаки, хаки, хаки");
+
+            #endregion
+
+            #region Alternating vowels, *
+
+            #endregion
+
+            #region Circled numbers, ①②
+
+            #endregion
+
+            #region Alternating ё
+
+            #endregion
+
+            return adapter;
+        }
+
+        public sealed class DeclensionFixture(string stem, string info) : FuncFixture<string>
+        {
+            [Obsolete(TestUtil.DeserCtor, true)] public DeclensionFixture() : this(null!, null!) { }
+
+            public string Stem { get; } = stem;
+            public string Info { get; } = info;
+
+            public string? Expected { get; private set; }
+
+            public DeclensionFixture Returns(string expected)
+            {
+                Expected = Expected is null ? expected : Expected + " // " + expected;
+                if (Expected?.Count(ch => ch == '/') == 6) MarkAsComplete();
+                return this;
+            }
+            public override void AssertResult(string? result)
+            {
+                Assert.NotNull(result);
+                Assert.Equal(Expected, result);
+            }
+            public override string ToString()
+                => $"{base.ToString()} {Stem}, {RussianAdjectiveInfo.Parse(Info)}";
+
+        }
+
+    }
+}
---
+++ Chasm.Grammar.Tests/Russian/Adjective.Declension.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Text;
+using Chasm.Collections;
+using Chasm.Grammar.Russian;
+using Xunit;
+
+namespace Chasm.Grammar.Tests
+{
+    public partial class RussianAdjectiveTests
+    {
+        [Theory, MemberData(nameof(CreateDeclensionFixtures))]
+        public void Declension(DeclensionFixture fixture)
+        {
+            var info = RussianAdjectiveInfo.Parse(fixture.Info);
+            RussianAdjective adjective = new RussianAdjective(fixture.Stem, info);
+
+            string t = info.ToString();
+            Output.WriteLine($"{adjective.Stem}, {info}\n");
+
+            StringBuilder sb = new();
+
+            string Decline(RussianGender gender, int num)
+                => adjective.Decline((RussianCase)num, gender == RussianGender.Common, new(gender, true));
+
+            // Decline for every gender, and then for plural
+            sb.AppendJoin(", ", Enumerable.Range(0, 7).Select(i => Decline(RussianGender.Neuter, i)));
+            sb.Append(" // ");
+            sb.AppendJoin(", ", Enumerable.Range(0, 7).Select(i => Decline(RussianGender.Masculine, i)));
+            sb.Append(" // ");
+            sb.AppendJoin(", ", Enumerable.Range(0, 7).Select(i => Decline(RussianGender.Feminine, i)));
+            sb.Append(" // ");
+            sb.AppendJoin(", ", Enumerable.Range(0, 7).Select(i => Decline(RussianGender.Common, i)));
+
+            string result = sb.ToString();
+
+            Output.WriteLine(result.Split(" // ").Join("\n"));
+
+            fixture.AssertResult(result);
+        }
+
+    }
+}
