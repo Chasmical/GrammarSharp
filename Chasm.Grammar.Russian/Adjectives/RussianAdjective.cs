1835ec82f8bcc1b74b8f29f1d03e51412ff683ec
Chasmical <kaz@chsm.dev>
2025-03-17 13:10:07 +0700

    ✨ Add FromStem constructor methods

--- Chasm.Grammar.Russian/Adjectives/RussianAdjective.cs
+++ Chasm.Grammar.Russian/Adjectives/RussianAdjective.cs
@@ -1,4 +1,6 @@
-﻿namespace Chasm.Grammar.Russian
+﻿using JetBrains.Annotations;
+
+namespace Chasm.Grammar.Russian
 {
     public sealed partial class RussianAdjective
     {
@@ -7,19 +9,22 @@
 
         public RussianAdjective(string word, RussianAdjectiveInfo info)
         {
-            string stem;
-
-            if (info.Declension.IsZero)
-                stem = word;
-            else
-            {
-                stem = info.Declension.ExtractStem(word, out bool isAdjReflexive);
-                if (isAdjReflexive) info.Flags |= RussianAdjectiveFlags.IsReflexive;
-            }
+            string stem = info.Declension.ExtractStem(word, out bool isAdjReflexive);
+            if (isAdjReflexive) info.Flags |= RussianAdjectiveFlags.IsReflexive;
 
             Stem = stem;
             Info = info;
         }
+
+        // ReSharper disable once UnusedParameter.Local
+        private RussianAdjective(string stem, RussianAdjectiveInfo info, bool _)
+        {
+            Stem = stem;
+            Info = info;
+        }
+
+        [Pure] public static RussianAdjective FromStem(string stem, RussianAdjectiveInfo info)
+            => new RussianAdjective(stem, info, false);
 
     }
 }
--- Chasm.Grammar.Russian/Nouns/RussianNoun.cs
+++ Chasm.Grammar.Russian/Nouns/RussianNoun.cs
@@ -1,4 +1,6 @@
-﻿namespace Chasm.Grammar.Russian
+﻿using JetBrains.Annotations;
+
+namespace Chasm.Grammar.Russian
 {
     public sealed partial class RussianNoun
     {
@@ -7,20 +9,23 @@
 
         public RussianNoun(string word, RussianNounInfo info)
         {
-            string stem;
-
-            if (info.Declension.IsZero)
-                stem = word;
-            else
-            {
-                stem = info.Declension.ExtractStem(word, out bool isAdjReflexive);
-                // Store "is reflexive adjective" flag in ExtraData (it's then retrieved during declension)
-                if (isAdjReflexive) info.Declension.IsReflexiveAdjective = isAdjReflexive;
-            }
+            string stem = info.Declension.ExtractStem(word, out bool isAdjReflexive);
+            // Store "is reflexive adjective" flag in ExtraData (it's then retrieved during declension)
+            if (isAdjReflexive) info.Declension.IsReflexiveAdjective = isAdjReflexive;
 
             Stem = stem;
             Info = info;
         }
+
+        // ReSharper disable once UnusedParameter.Local
+        private RussianNoun(string stem, RussianNounInfo info, bool _)
+        {
+            Stem = stem;
+            Info = info;
+        }
+
+        [Pure] public static RussianNoun FromStem(string stem, RussianNounInfo info)
+            => new RussianNoun(stem, info, false);
 
     }
 }
