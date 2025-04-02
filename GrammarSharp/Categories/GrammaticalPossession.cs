f42183909b1f89aea8488fd6c26c8cbcc12ed196
Chasmical <kaz@chsm.dev>
2025-04-02 16:58:40 +0700

    🚨 Suppress XML doc warnings for grammatical categories

--- GrammarSharp/Categories/GrammaticalPossession.cs
+++ GrammarSharp/Categories/GrammaticalPossession.cs
@@ -1,4 +1,6 @@
-﻿namespace GrammarSharp
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace GrammarSharp
 {
     public enum GrammaticalPossession
     {
